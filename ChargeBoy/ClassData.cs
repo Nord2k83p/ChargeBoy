2676cabf8d9354711791785408054e260dd1d12c
Nord2k83p <127845228+Nord2k83p@users.noreply.github.com>
2023-06-14 17:53:22 +0300

    Add files via upload

---
+++ ChargeBoy/ClassData.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChargeBoy
+{
+    class ClassData
+    {
+        //Хранилище данных о своих сделках
+        public class execord
+        {
+            public volatile string[] exename;
+            public volatile double[] exeprice;
+            public volatile int[] exequan;
+            public volatile string[] exedir;
+            public volatile int[] comment;
+            public volatile TimeSpan[] exetime;
+            public volatile long[] execode;
+            public volatile long[] exeorder;
+            public volatile int pos;
+            public execord(int sz)
+            {
+                pos = -1;
+                exename = new string[sz];
+                exeprice = new double[sz];
+                exequan = new int[sz];
+                exedir = new string[sz];
+                comment = new int[sz];
+                exetime = new TimeSpan[sz];
+                execode = new long[sz];
+                exeorder = new long[sz];
+            }
+            public void insertData(string stnameKey, string operationKey, double priceKey, double amountKey, TimeSpan timeKey, long numKey, long numOrderKey, int commentKey)
+            {
+                int npos = pos + 1;
+                exename[npos] = stnameKey;
+                exedir[npos] = operationKey;
+                exeprice[npos] = priceKey;
+                exequan[npos] = (int)amountKey;
+                exetime[npos] = timeKey;
+                execode[npos] = numKey;
+                exeorder[npos] = numOrderKey;
+                comment[npos] = commentKey;
+                pos++;
+            }
+            public long readData(currTradesData cdAE)
+            {
+                //if (pos == -1)
+                //{
+                for (int i = 0; i <= cdAE.pos; i++)
+                {
+                    if (cdAE.rev[i] != -1)
+                    {
+                        if (cdAE.numOrder[i] != 0)
+                        {
+                            pos++;
+                            exename[pos] = cdAE.name[i];
+                            exedir[pos] = cdAE.myOperation[i];
+                            exeprice[pos] = cdAE.price[i];
+                            exequan[pos] = (int)cdAE.vol[i];
+                            exetime[pos] = cdAE.time[i];
+                            execode[pos] = cdAE.num[i];
+                            exeorder[pos] = cdAE.numOrder[i];
+                            comment[pos] = cdAE.comment[i];
+                        }
+                    }
+                }
+                //}
+                return 0;
+            }
+        }
+
+        //Хранилище данных всех сделок
+        public class currTradesData
+        {
+            public volatile long[] rev;
+            public volatile TimeSpan[] time;
+            public volatile string[] name;
+            public volatile double[] price;
+            public volatile double[] vol;
+            public volatile string[] operation;
+            public volatile long[] num;
+            public volatile long[] numOrder;
+            public volatile string[] myOperation;
+            public volatile int[] comment;
+            public volatile int pos;
+            public volatile double[] timeLoc;
+
+            long lastrev;
+            //TimeSpan lasttime = new TimeSpan(10, 0, 0);
+            //private object lockInsertData = new object();
+
+            public currTradesData(int sz)
+            {
+                pos = -1;
+                rev = new long[sz];
+                time = new TimeSpan[sz];
+                name = new string[sz];
+                price = new double[sz];
+                vol = new double[sz];
+                operation = new string[sz];
+                num = new long[sz];
+                numOrder = new long[sz];
+                myOperation = new string[sz];
+                comment = new int[sz];
+                timeLoc = new double[sz];
+            }
+
+            public void insertData(long revKey, string stnameKey, string operationKey, double priceKey, double volumeKey, TimeSpan timeKey, long numKey, long numOrderKey, string myOperationKey, int commentKey)
+            {
+                //lock (lockInsertData)
+                //{
+                if (revKey == -1 || revKey > lastrev)
+                {
+                    int npos = pos + 1;
+                    rev[npos] = revKey;
+                    name[npos] = stnameKey;
+                    operation[npos] = operationKey;
+                    price[npos] = priceKey;
+                    vol[npos] = volumeKey;
+                    //if (timeKey.Hours == 0) time[npos] = lasttime;
+                    //else
+                    //{
+                    time[npos] = timeKey;
+                    //lasttime = timeKey;
+                    //}
+                    num[npos] = numKey;
+                    numOrder[npos] = numOrderKey;
+                    myOperation[npos] = myOperationKey;
+                    comment[npos] = commentKey;
+                    timeLoc[npos] = Form1.timer.ElapsedMilliseconds;
+                    pos++;
+                    if (revKey != -1) lastrev = revKey;
+                }
+                //}
+            }
+
+            public void saveData(string path)
+            {
+                if (pos >= 0)
+                {
+                    using (StreamWriter sw = new StreamWriter(path, false))
+                    {
+                        for (int i = 0; i <= pos; i++)
+                        {
+                            TimeSpan tL = (Form1.dt.TimeOfDay + new TimeSpan(0, 0, 0, 0, (int)timeLoc[i]));
+                            string s1 = Convert.ToString(rev[i]) + ";" +
+                                name[i] + ";" +
+                                operation[i] + ";" +
+                                Convert.ToString(price[i]).Replace(',', '.') + ";" +
+                                Convert.ToString(Convert.ToUInt32(vol[i])) + ";" +
+                                time[i] + ";" +
+                                Convert.ToString(num[i]) + ";" +
+                                Convert.ToString(numOrder[i]) + ";" +
+                                Convert.ToString(myOperation[i]) + ";" +
+                                Convert.ToString(comment[i]) + ";" +
+                                tL.ToString() + ";";
+                            sw.WriteLine(s1);
+                        }
+                    }
+                }
+            }
+            public long readData(string path)
+            {
+                if (File.Exists(path))
+                {
+                    using (StreamReader sr = new StreamReader(path))
+                    {
+                        while (true)
+                        {
+                            string s = sr.ReadLine();
+                            if (s == null || s == "") break;
+                            string[] sarray = s.Split(';');
+                            if (Convert.ToInt32(sarray[0]) != -1)
+                            {
+                                pos++;
+                                rev[pos] = Convert.ToInt32(sarray[0]);
+                                if (rev[pos] != -1) lastrev = rev[pos];
+                                name[pos] = sarray[1];
+                                operation[pos] = sarray[2];
+                                price[pos] = (float)Convert.ToDouble(sarray[3].Replace('.', ','));
+                                vol[pos] = (float)Convert.ToDouble(sarray[4]);
+                                time[pos] = Convert.ToDateTime(sarray[5]).TimeOfDay;
+                                num[pos] = Convert.ToInt64(sarray[6]);
+                                numOrder[pos] = Convert.ToInt64(sarray[7]);
+                                myOperation[pos] = sarray[8];
+                                comment[pos] = Convert.ToInt32(sarray[9]);
+                            }
+                        }
+                    }
+                    return lastrev;
+                }
+                return 0;
+            }
+        }
+    }
+}
---
+++ ChargeBoy/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Windows;
+
+namespace ChargeBoy
+{
+    /// <summary>
+    /// Стартовая форма для запуска робота
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        // Импорт библиотеки для уменьшения кванта времени Windows
+        [DllImport("winmm.dll")]
+        internal static extern uint timeBeginPeriod(uint period);
+        [DllImport("winmm.dll")]
+        internal static extern uint timeEndPeriod(uint period);
+
+        public static int nThr = 0;
+        static public DateTime dt = DateTime.Now;
+        //Прецизионный таймер
+        static volatile public System.Diagnostics.Stopwatch timer = new System.Diagnostics.Stopwatch();
+        //Флаг запуска коннектора к Плаза2
+        public static bool plaza2 = false;
+        //Флаг подключения стакана
+        public static bool glassP2 = false;
+        //Код клиента для Плаза2
+        public static string client_code_P2;
+
+        //Структура для записи задаваемых параметров робота (см.ниже расшифровку)
+        struct EntryData
+        {
+            public string futName;
+            public string dollName;
+            public string classFut;
+            public string baseContract;
+            public int smaPeriod;
+            public int amountFut;
+            public int amountFutMax;
+            public int normsec;
+            public double ks;
+            public int Nstock;
+            public double koeff;
+            public int kTreshold;
+            public int priceStep;
+            public bool hf;
+            public int HFT;
+            public string timeEnd;
+            public bool nr;
+            public int sec;
+            public bool th;
+        }
+        EntryData[] ed = new EntryData[5];
+        static object[] cm = new object[5];
+
+        //Структура для записи результатов робота
+        public static volatile resultData[] res = new resultData[5];
+        Thread[] th = new Thread[5];
+        Thread ddeserv;
+        Thread ddeclient;
+        Thread output;
+        Thread thcp2;
+        Thread thcp2tr;
+        volatile ClassDdeServer ds;
+        FormOutput fo;
+        //Структура для записи собственных сделок
+        public static volatile execord exec = new execord(40000);
+        //Структура для записи коллбэка
+        public static volatile callbackTransData cbTrD = new callbackTransData(150000);
+        //Структура для записи всех сделок по фьючерсам
+        public static volatile currTradesData currDataFut = new currTradesData(1000000);
+        //Структура для записи всех сделок по бумагам, входящим в индекс
+        public static volatile currTradesData currDataInd = new currTradesData(1000000);
+        //public static dataGlassRaw glRaw = new dataGlassRaw(10000000);
+        // Класс для формирования стакана
+        public static glass GL = new glass(20);
+        //Структура для записи транзакций
+        public static volatile ComDataTransP2 cdTransP2 = new ComDataTransP2(150000);
+        private ClassFunc.TransactionReplyCallback callbackTransactionReply = null;
+        private ClassFunc.trade_status_callback trade_callback = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
